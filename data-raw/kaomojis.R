76515a370e6a84a8221ad58465fdbf953f9ee3c7
mchuk <mchuk@its.jnj.com>
2021-01-29 20:07:16 -0800

    different structure

--- data-raw/kaomojis.R
+++ data-raw/kaomojis.R
@@ -8,10 +8,26 @@ kaomojis_categories <- raw_html %>%
   html_nodes(css = "h3") %>%
   html_text()
 
-kaomojis <- raw_html %>%
+all_kaomojis <- raw_html %>%
   html_table(header = NA) %>%
   .[3:41] %>%
-  setNames(kaomojis_categories) %>%
-  lapply(unlist)
+  setNames(kaomojis_categories)
+
+normal_kaomojis <- all_kaomojis %>%
+  head(-1) %>%
+  lapply(unlist) %>%
+  lapply(unname)
+
+special_kaomojis <- all_kaomojis %>%
+  tail(1) %>%
+  unlist(recursive = FALSE)
+
+special_kaomojis <- list(Special = setNames(
+  object = as.list(special_kaomojis$Special.X1),
+  nm     = special_kaomojis$Special.X2
+))
+
+
+kaomojis <- c(normal_kaomojis, special_kaomojis)
 
 usethis::use_data(kaomojis, internal = F, overwrite = T)
